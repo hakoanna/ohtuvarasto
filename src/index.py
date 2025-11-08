c339505c461448ddef1087fdc94d51b70a599dc9
Anna Hakosalo <anna.hakosalo@outlook.com>
2025-11-08 11:02:01 +0200

    testing pylint actions tests after fixing

--- src/index.py
+++ src/index.py
@@ -10,8 +10,6 @@ def main():
     print("Otetaan 3.14")
     mehua.ota_varastosta(3.14)
     print(f"Mehuvarasto: {mehua}")
-    print("rivi 10")
-    print("rivi 11")
 
 if __name__ == "__main__":
     main()
